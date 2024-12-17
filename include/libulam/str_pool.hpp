c7441b1062cba126d3080c28c8c605a64be1a9d6
sergei sh <managerzf168@gmail.com>
2024-12-18 02:23:45 +0300

    str pool stacking for future repl purposes (untested)

--- include/libulam/str_pool.hpp
+++ include/libulam/str_pool.hpp
@@ -17,7 +17,7 @@ public:
     StrPoolBase& operator=(const StrPoolBase&) = delete;
 
     virtual str_id_t put(const std::string_view str, bool copy = true) = 0;
-    const std::string_view get(str_id_t id) const;
+    virtual const std::string_view get(str_id_t id) const;
 
 protected:
     using PairT = std::pair<str_id_t, const std::string_view>;
@@ -30,18 +30,24 @@ protected:
 
 class UniqStrPool : public StrPoolBase {
 public:
+    explicit UniqStrPool(UniqStrPool* parent = {});
+
     bool has(const std::string_view str) const;
     str_id_t id(const std::string_view str) const;
 
-    str_id_t put(const std::string_view str, bool copy = true);
+    str_id_t put(const std::string_view str, bool copy = true) override;
+    const std::string_view get(str_id_t id) const override;
 
 private:
+    UniqStrPool* _parent;
+    str_id_t _offset{0};
+    bool _is_locked{false}; // marks parent pool as locked (assert check only)
     std::unordered_map<std::string_view, str_id_t> _map;
 };
 
 class StrPool : public StrPoolBase {
 public:
-    virtual str_id_t put(const std::string_view str, bool copy = true);
+    str_id_t put(const std::string_view str, bool copy = true) override;
 };
 
 } // namespace ulam
--- src/str_pool.cpp
+++ src/str_pool.cpp
@@ -19,22 +19,49 @@ StrPoolBase::PairT StrPoolBase::store(const std::string_view str, bool copy) {
 
 // UniqStrPool
 
+UniqStrPool::UniqStrPool(UniqStrPool* parent):
+    StrPoolBase{}, _parent{parent} {
+    if (_parent) {
+        _offset = _parent->_offset + _parent->_index.size();
+        _parent->_is_locked = true; // lock parent
+    }
+}
+
 bool UniqStrPool::has(const std::string_view str) const {
-    return _map.count(str) == 1;
+    return _map.count(str) == 1 || (_parent && _parent->has(str));
 }
 
 str_id_t UniqStrPool::id(const std::string_view str) const {
+    if (_parent) {
+        auto str_id = _parent->id(str);
+        if (str_id != NoStrId)
+            return str_id;
+    }
     auto it = _map.find(str);
-    return (it != _map.end()) ? it->second : NoStrId;
+    return (it != _map.end()) ? it->second + _offset : NoStrId;
+}
+
+const std::string_view UniqStrPool::get(str_id_t id) const {
+    if (id < _offset) {
+        assert(_parent);
+        return _parent->get(id);
+    }
+    return StrPoolBase::get(id - _offset);
 }
 
 str_id_t UniqStrPool::put(const std::string_view str, bool copy) {
+    assert(!_is_locked);
+    if (_parent) {
+        auto str_id = _parent->id(str);
+        if (str_id != NoStrId)
+            return str_id;
+    }
     auto it = _map.find(str);
     if (it != _map.end())
         return it->second;
     auto stored = store(str, copy);
     _map[stored.second] = stored.first;
-    return stored.first;
+    return stored.first + _offset;
 }
 
 // StrPool
