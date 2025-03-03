4d92de8caaf203dee4b9b82e86f11522d78375d3
sergei sh <managerzf168@gmail.com>
2025-03-03 21:25:06 +0300

    mangler Atom type

--- src/semantic/mangler.cpp
+++ src/semantic/mangler.cpp
@@ -68,6 +68,8 @@ void Mangler::write_mangled(std::ostream& os, Ref<const Type> type) {
                 write_mangled(os, rval);
             });
         }
+    } else if (type->is_atom()) {
+        os << "a";
     } else {
         assert(type->is_prim());
         detail::write_leximited(os, builtin_type_code(type->bi_type_id()));
