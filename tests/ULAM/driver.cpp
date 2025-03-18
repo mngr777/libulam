1ef19bdc93dc530b52d078c63f792d2e54030a22
sergei sh <managerzf168@gmail.com>
2025-03-18 14:05:09 +0300

    tests unskipped

--- tests/ULAM/driver.cpp
+++ tests/ULAM/driver.cpp
@@ -67,9 +67,6 @@ int main(int argc, char** argv) {
         std::set<std::string> skip = {
             "t3233_test_compiler_elementandquarkarray_ew.test", // aref called by name
             "t3241_test_compiler_unarymod.test", // invalid implicit cast?
-            "t3357_test_compiler_elementandquarkswclassargs_lhsconstantcompare.test", // behave()
-            "t3358_test_compiler_elementandquarkswclassargs_lhsconstantlogicaland.test", // behave()
-            "t3361_test_compiler_elementandquarkswclassargs_memberconstantasfunccallarg.test", // behave()
             "t3484_test_compiler_elementandquark_caarray_ambiguousfunc_issue.test", // TODO: aref can have overloads
             "t3494_test_compiler_divideandmodmixedtypes.test", // carrying consteval flag through props?
             "t3501_test_compiler_elementandquarkcaarray_unsignedindex_issue.test", // ambiguous funcall as intended
