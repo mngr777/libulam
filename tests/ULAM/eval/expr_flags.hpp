dc8243730cd84330650fbc0bf2992b1e6b6d6ed1
sergei sh <managerzf168@gmail.com>
2025-04-13 21:06:48 +0300

    postfix: expr res flag namespace

--- tests/ULAM/eval/expr_flags.hpp
+++ tests/ULAM/eval/expr_flags.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <libulam/sema/expr_res.hpp>
 
+namespace exp {
 static constexpr ulam::sema::ExprRes::flags_t ImplCast = 1;
 static constexpr ulam::sema::ExprRes::flags_t ExplCast = 1 << 1;
 static constexpr ulam::sema::ExprRes::flags_t Self = 1 << 2;
 static constexpr ulam::sema::ExprRes::flags_t ExtMemberAccess = 1 << 3;
+}
