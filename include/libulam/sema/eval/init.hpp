e34aad4b23adc08962fefe925074f317fc772135
sergei sh <managerzf168@gmail.com>
2025-04-14 20:51:23 +0300

    postfix: init values

--- include/libulam/sema/eval/init.hpp
+++ include/libulam/sema/eval/init.hpp
@@ -4,6 +4,7 @@
 #include <libulam/detail/variant.hpp>
 #include <libulam/memory/ptr.hpp>
 #include <libulam/sema/eval/helper.hpp>
+#include <libulam/sema/expr_res.hpp>
 #include <libulam/semantic/type.hpp>
 #include <libulam/semantic/value.hpp>
 #include <libulam/semantic/value/types.hpp>
@@ -14,34 +15,38 @@ class EvalVisitor;
 
 class EvalInit : public EvalHelper {
 public:
-    using EvalRes = std::pair<Value, bool>;
-
     using EvalHelper::EvalHelper;
 
     // {dimensions, is_array}
     std::pair<ArrayDimList, bool>
     array_dims(unsigned num, Ref<ast::InitValue> init);
 
-    virtual EvalRes eval_init(Ref<Type> type, Ref<ast::InitValue> init);
+    virtual ExprRes eval_init(Ref<Type> type, Ref<ast::InitValue> init);
 
 protected:
     using Variant = ast::InitValue::ItemT;
     static_assert(std::is_same<Variant, ast::InitList::ItemT>());
     static_assert(std::is_same<Variant, ast::InitMap::ItemT>());
 
-    EvalRes eval_v(Ref<Type> type, Variant& v, unsigned depth);
+    ExprRes eval_v(Ref<Type> type, Variant& v, unsigned depth);
 
-    EvalRes eval_expr(Ref<Type> type, Ref<ast::Expr> expr, unsigned depth);
-    EvalRes eval_list(Ref<Type> type, Ref<ast::InitList> list, unsigned depth);
-    EvalRes eval_map(Ref<Type> type, Ref<ast::InitMap> map, unsigned depth);
+    virtual ExprRes eval_expr(Ref<Type> type, Ref<ast::Expr> expr, unsigned depth);
+    virtual ExprRes eval_list(Ref<Type> type, Ref<ast::InitList> list, unsigned depth);
+    virtual ExprRes eval_map(Ref<Type> type, Ref<ast::InitMap> map, unsigned depth);
 
-    EvalRes
+    virtual ExprRes
     eval_class_list(Ref<Class> cls, Ref<ast::InitList> list, unsigned depth);
-    EvalRes eval_array_list(
+    virtual ExprRes eval_array_list(
         Ref<ArrayType> array_type, Ref<ast::InitList> list, unsigned depth);
 
-    EvalRes
+    virtual ExprRes
     eval_class_map(Ref<Class> cls, Ref<ast::InitMap> map, unsigned depth);
+
+    virtual ExprRes make_array(Ref<ArrayType> array_type);
+    virtual ExprRes array_set(ExprRes&& array, array_idx_t idx, ExprRes&& item);
+
+    virtual ExprRes make_obj(Ref<Class> cls);
+    virtual ExprRes obj_set(ExprRes&& obj, Ref<Prop> prop, ExprRes&& prop_res);
 };
 
 } // namespace ulam::sema
--- tests/ULAM/eval/init.hpp
+++ tests/ULAM/eval/init.hpp
@@ -1,6 +1,31 @@
+#include <libulam/memory/ptr.hpp>
 #include <libulam/sema/eval/init.hpp>
+#include <libulam/sema/expr_res.hpp>
+#include <libulam/semantic/type/class.hpp>
+#include <libulam/semantic/type/class/prop.hpp>
 
 class EvalInit : public ulam::sema::EvalInit {
 public:
     using ulam::sema::EvalInit::EvalInit;
+
+protected:
+    virtual ulam::sema::ExprRes eval_array_list(
+        ulam::Ref<ulam::ArrayType> array_type,
+        ulam::Ref<ulam::ast::InitList> list,
+        unsigned depth);
+
+    virtual ulam::sema::ExprRes
+    make_array(ulam::Ref<ulam::ArrayType> array_type);
+
+    virtual ulam::sema::ExprRes array_set(
+        ulam::sema::ExprRes&& array,
+        ulam::array_idx_t idx,
+        ulam::sema::ExprRes&& item);
+
+    // virtual ulam::sema::ExprRes make_obj(ulam::Ref<ulam::Class> cls);
+
+    // virtual ulam::sema::ExprRes obj_set(
+    //     ulam::sema::ExprRes&& obj,
+    //     ulam::Ref<ulam::Prop> prop,
+    //     ulam::sema::ExprRes&& prop_res);
 };
